6037f8df6f9b7f8e640eea7656e2309387ab39ca
Kelevan69 <kelevan@yandex.ru>
2024-11-22 20:27:00 +0300

    JPA Repositories

--- src/main/java/ru/netology/orm/Person.java
+++ src/main/java/ru/netology/orm/Person.java
@@ -1,36 +1,62 @@
 package ru.netology.orm;
 
 import jakarta.persistence.*;
-import ru.netology.orm.PersonId;
 
 @Entity
 @Table(name = "PERSONS")
 public class Person {
 
-    @EmbeddedId
-    private PersonId id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @Column(name = "phone_number")
+    private String name;
+    private String surname;
+    private int age;
     private String phoneNumber;
-
-    @Column(name = "city_of_living")
     private String cityOfLiving;
 
     public Person() {
     }
 
-    public Person(PersonId id, String phoneNumber, String cityOfLiving) {
-        this.id = id;
+    public Person(String name, String surname, int age, String phoneNumber, String cityOfLiving) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
         this.phoneNumber = phoneNumber;
         this.cityOfLiving = cityOfLiving;
     }
 
-    public PersonId getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(PersonId id) {
+    public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public String getPhoneNumber() {
